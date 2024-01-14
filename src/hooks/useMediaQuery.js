28dcd5aba1c60bfeef55952f5b6c8d27a1dcb99f
hxgdzyuyi <hxgdzyuyi@gmail.com>
2024-01-14 21:25:34 +0800

    feat: add hooks/useMediaQuery

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,26 @@
+// Copy from https://github.com/uidotdev/usehooks/blob/main/index.js
+//
+
+export function useMediaQuery(query) {
+  const subscribe = React.useCallback(
+    (callback) => {
+      const matchMedia = window.matchMedia(query);
+
+      matchMedia.addEventListener("change", callback);
+      return () => {
+        matchMedia.removeEventListener("change", callback);
+      };
+    },
+    [query]
+  );
+
+  const getSnapshot = () => {
+    return window.matchMedia(query).matches;
+  };
+
+  const getServerSnapshot = () => {
+    throw Error("useMediaQuery is a client-only hook");
+  };
+
+  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+}
